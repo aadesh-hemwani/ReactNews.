bf15a95deea5fb976f0407cbbe9da598a0e2396b
aadesh-hemwani <aadesh.hemwani@gmail.com>
2020-07-17 20:01:40 +0530

    Updates

---
+++ precache-manifest.44a23258332b2863f5d9e1ca1c9ce307.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18fbdece493b3ef94eae97d42dfbdf1c",
+    "url": "/ReactNews./index.html"
+  },
+  {
+    "revision": "424c05ef8f2e0bc9243b",
+    "url": "/ReactNews./static/css/2.8fd11fc7.chunk.css"
+  },
+  {
+    "revision": "c99312c2410b4afb0391",
+    "url": "/ReactNews./static/css/main.b4d450cc.chunk.css"
+  },
+  {
+    "revision": "424c05ef8f2e0bc9243b",
+    "url": "/ReactNews./static/js/2.e8659067.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactNews./static/js/2.e8659067.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c99312c2410b4afb0391",
+    "url": "/ReactNews./static/js/main.fb12e22d.chunk.js"
+  },
+  {
+    "revision": "af8f3946c8050f2c0a30",
+    "url": "/ReactNews./static/js/runtime-main.af3f9fc6.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/ReactNews./static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/ReactNews./static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/ReactNews./static/media/revicons.2feb69cc.eot"
+  }
+]);
